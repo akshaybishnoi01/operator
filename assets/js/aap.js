9887e63d0d041e8c1a7d6adf8784fc8b142f1172
akshaybishnoi01 <akshaybishnoi0008@gmail.com>
2024-05-31 16:24:38 +0530

    update

---
+++ assets/js/aap.js
@@ -0,0 +1,34 @@
+function someName(num1, num2) {
+    // bunch of code as needed...
+    var a = num1 + 4 + num2;
+    console.log(a);
+}
+someName(4 , 4)
+
+
+
+
+//=======Arithmetic Operators=========\\
+    let a = 2;
+         let b = 4;
+         
+        console.log("a + b =" , a + b);
+        console.log("a - b =" , a - b);
+        console.log("a / b =" , a / b);
+        console.log("a * b =" , a * b);
+        console.log("a % b =" , a % b);
+        console.log("a ** b =" , a ** b);
+ 
+
+let c = 4;
+let d = 6;
+console.log( c += d);
+console.log(c -= d);
+
+        let e = 6;
+        let f = 2;
+        let cond1 = e < f ;
+        let cond2 = e == 6;
+        console.log( cond1 && cond2);
+        console.log( cond1 || cond2);
+        console.log("!(4<2) =" , !(4<2));
